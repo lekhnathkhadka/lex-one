61b8920faab5602825a19d97504db65870d426c1
LEARNER AI <lekhnathkhadka1@gmail.com>
2026-08-24 20:17:06 +0545

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://ymeetoihaszqswvhdnmm.supabase.co";
 
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_B07z940MOZc1vLijvxo7DA_c3Mh_ixb";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
